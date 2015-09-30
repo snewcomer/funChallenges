6f65d6ce0dbc2e5c21571e7e1a016f7b7939412b
Scott Newcomer <snewcomer@users.noreply.github.com>
2015-09-29 21:48:03 -0700

    Update scrambleWords.js

--- scrambleWords.js
+++ scrambleWords.js
@@ -1,8 +1,44 @@
+//correct sol'n
+function scramble(str1, str2) {
+  let dict = {};
+  let dicty = {};
+  let memo = true;
+  let count = 0;
+  for (let y=0; y < str1.length; y++) {
+    dicty[str1[y]] = dicty[str1[y]] + 1 || 1;
+  }
+  for (let x=0; x < str2.length; x++) {
+    if (str2[x] in dicty && dicty[str2[x]] > 0) {
+      dict[str2[x]] = dict[str2[x]] + 1 || 1;
+      dicty[str2[x]] -= 1;
+      memo = true && memo;
+    }
+    if (dict[str2[x]] < 0) {
+      memo = false;
+    }
+  }
+  Object.keys(dict).forEach((key, indx) => {
+    count += dict[key];
+  });
+  return memo && count === str2.length;
+}
+
+
 function scramble(str1, str2) {
   let str2Array = str2.split('');
   return bool = str2Array.reduce((memo, letter) => {
     let str2RegExp = new RegExp('[' + letter + ']');
     return memo && str2RegExp.test(str1);  
+  }, true);
+}
+
+function scramble(str1, str2) {
+  let str2Array = str2.split('');
+  let dict = {};
+  return bool = str2Array.reduce((memo, letter) => {
+    let indx = dict[letter] || str1.indexOf(letter);
+    dict[letter] = indx
+    return memo && indx > -1;  
   }, true);
 }
 
