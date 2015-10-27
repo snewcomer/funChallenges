c2e679ea54367eb9ce2050afe11cb3bd4af48a5f
Scott Newcomer <snewcomer@users.noreply.github.com>
2015-10-27 12:44:49 -0700

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -18,3 +18,46 @@ var allAnagrams = function(arr) {
 }
 
 console.log(allAnagrams(arr));
+
+
+function generateAnagrams(word) {
+
+    if (word.length < 2) {
+
+        return [word];
+
+    } else {
+
+        // By declaring all variables outside of the loop,
+        // we improve efficiency, avoiding the needless
+        // declarations each time.
+
+        var anagrams = [];
+        var before, focus, after;
+        var shortWord, subAnagrams, newEntry;
+
+        for (var i = 0; i < word.length; i++) {
+
+            before = word.slice(0, i);
+            focus = word[i];
+            after = word.slice(i + 1, word.length + 1);
+            shortWord = before + after;
+            subAnagrams = generateAnagrams(shortWord);
+            for (var j = 0; j < subAnagrams.length; j++){
+
+                newEntry = focus + subAnagrams[j];
+                anagrams.push(newEntry);
+
+            }
+
+        }
+
+        return anagrams;
+
+    }
+
+}
+
+var word = 'abc';
+var result = generateAnagrams(word);
+console.log(result)
