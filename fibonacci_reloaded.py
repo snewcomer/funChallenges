e9ed567bed64b17f2beed7d5ffdbd2c49f7855d5
Scott Newcomer <snewcomer@users.noreply.github.com>
2015-11-23 23:32:05 -0800

    Update fibonacci_reloaded.py

--- fibonacci_reloaded.py
+++ fibonacci_reloaded.py
@@ -9,3 +9,8 @@ def fib(n):
     x = list(gen_fib(n))
     return x[n-1]
         
+#OR
+def good fibonacci(n):
+”””Return pair of Fibonacci numbers, F(n) and F(n-1).””” if n <= 1:
+return (n,0) else:
+(a, b) = good fibonacci(n−1) return (a+b, a)
